19286d709385cd2149f7324ce44171b633a85da6
songjin <songjin@localhost.localdomain>
2026-02-03 09:49:15 +0100

    aaa

--- resistor.cpp
+++ resistor.cpp
@@ -5,59 +5,35 @@ namespace electronics{
   uint64_t Resistor::resistance(Unit unit)const{
 
     uint64_t value=0;
-    uint64_t value2;
+
     switch(unit){
      case Unit::microohm:
-        value2=1000000000000000000;
+        value=1;
         break;
       case Unit::milliohm:
-        value2 =1000000000000000;
+        value =1000;
         break;
       case Unit::ohm:
-        value2 =1000000000000;
+        value =1000000;
         break;
       case Unit::kiloohm:
-        value2 =1000000000;
+        value =1000000000;
         break;
       case Unit::megaohm:
-        value2 = 1000000;
+        value = 1000000000000;
         break;
       case Unit::gigaohm:
-        value2 = 1000;
+        value = 1000000000000000;
         break;
       case Unit::teraohm:
-        value2 =1;
+        value =1000000000000000000;
         break;
 
     }
 
-    switch(unit_){
-      case Unit::microohm:
-        value2=1000000000000000000;
-        break;
-      case Unit::milliohm:
-        value2 =1000000000000000;
-        break;
-      case Unit::ohm:
-        value2 =1000000000000;
-        break;
-      case Unit::kiloohm:
-        value2 =1000000000;
-        break;
-      case Unit::megaohm:
-        value2 = 1000000;
-        break;
-      case Unit::gigaohm:
-        value2 = 1000;
-        break;
-      case Unit::teraohm:
-        value2 =1;
-        break;
-
-    }
     if(wert_==0){return 0;}else{
-    double value3 = value/value2;
-    return static_cast<int64_t>(wert_*value3);
+    double value2 = double(resistance()/value);
+    return static_cast<int64_t>(value2);
     }
   };
 
